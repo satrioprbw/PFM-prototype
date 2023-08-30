2765a56f8b822a0e7819558c016c9bce0025023a
Muhammad Satrio Prabowo <muhammad.satrio.prabowo@bni.co.id>
2023-08-30 11:45:16 +0700

    add categories and accounts

---
+++ data.js
@@ -0,0 +1,134 @@
+const categories = {
+  "Expenses": {
+    "Food & Drink" : {
+      "icon" : "KoubeiFill",
+      "color" : "red",
+      "subcategories" : {
+        "Groceries" : {
+          "icon" : "CompassOutline",
+          "color" : "red"
+        },
+        "Snack" : {
+          "icon" : "CompassOutline",
+          "color" : "red"
+        },
+        "Restaurant" : {
+          "icon" : "CompassOutline",
+          "color" : "red"
+        }
+      }
+    },
+    "Housing" : {
+      "icon" : "KoubeiFill",
+      "color" : "green",
+      "subcategories" : {
+        "Rent" : {
+          "icon" : "CompassOutline",
+          "color" : "green"
+        },
+        "Electricity" : {
+          "icon" : "CompassOutline",
+          "color" : "green"
+        },
+        "Repair" : {
+          "icon" : "CompassOutline",
+          "color" : "green"
+        }
+      }
+    },
+    "Transportation" : {
+      "icon" : "TruckOutline",
+      "color" : "purple",
+      "subcategories" : {
+        "Fuel" : {
+          "icon" : "TruckOutline",
+          "color" : "purple"
+        },
+        "Public Transportation" : {
+          "icon" : "TruckOutline",
+          "color" : "purple"
+        },
+        "Taxi" : {
+          "icon" : "TruckOutline",
+          "color" : "purple"
+        }
+      }
+    },
+    "Life & Entertainment" : {
+      "icon" : "GiftOutline",
+      "color" : "yellow",
+      "subcategories" : {
+        "Hobbies" : {
+          "icon" : "GiftOutline",
+          "color" : "yellow"
+        },
+        "Travelling" : {
+          "icon" : "GiftOutline",
+          "color" : "yellow"
+        },
+        "Streaming" : {
+          "icon" : "GiftOutline",
+          "color" : "yellow"
+        }
+      }
+    },
+    "Shopping" : {
+      "icon" : "HandPayCircleOutline",
+      "color" : "pink",
+      "subcategories" : {
+        "Fashion" : {
+          "icon" : "HandPayCircleOutline",
+          "color" : "pink"
+        },
+        "Electronics" : {
+          "icon" : "HandPayCircleOutline",
+          "color" : "pink"
+        },
+        "Pets" : {
+          "icon" : "HandPayCircleOutline",
+          "color" : "pink"
+        }
+      }
+    }
+  },
+  "Incomes": {
+    "Salary" : {
+      "icon" : "ReceivePaymentOutline",
+      "color" : "blue"
+    },
+    "Investment" : {
+      "icon" : "HistogramOutline",
+      "color" : "orange"
+    },
+    "Bonus" : {
+      "icon" : "MailOutline",
+      "color" : "brown"
+    },
+    "Side Job" : {
+      "icon" : "TeamOutline",
+      "color" : "yellow"
+    },
+    "Lottery" : {
+      "icon" : "FlagOutline",
+      "color" : "green"
+    }
+  }
+}
+
+const accounts = {
+  "Card" : {
+    "balance" : 5000000,
+    "icon" : "BankcardOutline",
+    "color" : "blue"
+  },
+  "Cash" : {
+    "balance" : 1000000,
+    "icon" : "PayCircleOutline",
+    "color" : "green"
+  },
+  "Savings" : {
+    "balance" : 10000000,
+    "icon" : "BillOutline",
+    "color" : "purple"
+  }
+}
