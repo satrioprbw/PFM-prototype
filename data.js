6485792630406e0df0c4f3c7ad4e17fc7f8a0a29
Muhammad Satrio Prabowo <muhammad.satrio.prabowo@bni.co.id>
2023-08-30 15:29:40 +0700

    added transactions june

--- data.js
+++ data.js
@@ -132,3 +132,166 @@ const accounts = {
     "color" : "purple"
   }
 }
+
+const transactions = [
+  {
+    "AccountId" : "Card",
+    "CategoryId" : "Shopping",
+    "SubcategoryId" : "Pets",
+    "amount" : 50000,
+    "transactionDate": "04-06-2023",
+    "transactionType" : "Expense"
+  },
+  {
+    "AccountId" : "Savings",
+    "CategoryId" : "Shopping",
+    "SubcategoryId" : "Electronics",
+    "amount" : 120000,
+    "transactionDate": "11-06-2023",
+    "transactionType" : "Expense"
+  },
+  {
+    "AccountId" : "Cash",
+    "CategoryId" : "Shopping",
+    "SubcategoryId" : "Fashion",
+    "amount" : 300000,
+    "transactionDate": "18-06-2023",
+    "transactionType" : "Expense"
+  },
+  {
+    "AccountId": "Cash",
+    "CategoryId": "Food & Drink",
+    "SubcategoryId": "Groceries",
+    "amount": 85000,
+    "transactionDate": "05-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Savings",
+    "CategoryId": "Food & Drink",
+    "SubcategoryId": "Snack",
+    "amount": 120000,
+    "transactionDate": "12-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Card",
+    "CategoryId": "Food & Drink",
+    "SubcategoryId": "Restaurant",
+    "amount": 75000,
+    "transactionDate": "19-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Cash",
+    "CategoryId": "Housing",
+    "SubcategoryId": "Rent",
+    "amount": 950000,
+    "transactionDate": "06-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Savings",
+    "CategoryId": "Housing",
+    "SubcategoryId": "Electricity",
+    "amount": 140000,
+    "transactionDate": "13-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Card",
+    "CategoryId": "Housing",
+    "SubcategoryId": "Repair",
+    "amount": 75000,
+    "transactionDate": "20-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Cash",
+    "CategoryId": "Transportation",
+    "SubcategoryId": "Fuel",
+    "amount": 250000,
+    "transactionDate": "25-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Savings",
+    "CategoryId": "Transportation",
+    "SubcategoryId": "Public Transportation",
+    "amount": 110000,
+    "transactionDate": "26-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Card",
+    "CategoryId": "Transportation",
+    "SubcategoryId": "Taxi",
+    "amount": 55000,
+    "transactionDate": "27-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Cash",
+    "CategoryId": "Life & Entertainment",
+    "SubcategoryId": "Hobbies",
+    "amount": 250000,
+    "transactionDate": "07-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Savings",
+    "CategoryId": "Life & Entertainment",
+    "SubcategoryId": "Travelling",
+    "amount": 110000,
+    "transactionDate": "14-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Card",
+    "CategoryId": "Life & Entertainment",
+    "SubcategoryId": "Streaming",
+    "amount": 55000,
+    "transactionDate": "21-06-2023",
+    "transactionType": "Expense"
+  },
+  {
+    "AccountId": "Card",
+    "CategoryId": "Salary",
+    "SubcategoryId": "",
+    "amount": 15000000,
+    "transactionDate": "01-06-2023",
+    "transactionType": "Income"
+  },
+  {
+    "AccountId": "Savings",
+    "CategoryId": "Investment",
+    "SubcategoryId": "",
+    "amount": 1500000,
+    "transactionDate": "08-06-2023",
+    "transactionType": "Income"
+  },
+  {
+    "AccountId": "Cash",
+    "CategoryId": "Bonus",
+    "SubcategoryId": "",
+    "amount": 3000000,
+    "transactionDate": "15-06-2023",
+    "transactionType": "Income"
+  },
+  {
+    "AccountId": "Savings",
+    "CategoryId": "Side Job",
+    "SubcategoryId": "",
+    "amount": 3000000,
+    "transactionDate": "22-06-2023",
+    "transactionType": "Income"
+  },
+  {
+    "AccountId": "Card",
+    "CategoryId": "Lottery",
+    "SubcategoryId": "",
+    "amount": 2000000,
+    "transactionDate": "29-06-2023",
+    "transactionType": "Income"
+  },
+]
